e4fb1a81c9b1214f93013e1883743742c5121857
gusXvo <gusXvo@gmail.com>
2016-04-24 19:52:25 -0500

    Added files via upload

---
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "conio.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello Wolrd");
+	_getch();
+
+	return 0;
+}
+
